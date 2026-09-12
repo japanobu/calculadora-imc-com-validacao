3337711f458b01e7018f962bf717fe82037d3d01
japanobu <viniciusnoboru@gmail.com>
2026-09-12 01:01:44 -0300

    Alterando código para melhor desempenho
    Alterando o código

--- calculadora.cpp
+++ calculadora.cpp
@@ -12,25 +12,26 @@ int main()
  std::cout << "Insira o valor da sua altura em metros:" << std::endl;
  std::cin >> altura;
  IMC = peso/pow(altura,2);
+ std::cout << "Seu IMC é:" << IMC << std::endl;
  if (IMC < 18.5){
      std::cout << "Abaixo do peso ideal" << std::endl;
  }
- if ( 18.5<=IMC && IMC<= 24.9){
+ else if (IMC <= 24.9){
      std::cout << "Peso normal" << std::endl;
  }
  
- if (25<= IMC && IMC <= 29.9){
+else if (IMC <= 29.9){
      std::cout << "Sobrepeso" << std::endl;
  }
- if (30<=IMC && IMC <= 34.9){
+ else if (IMC <= 34.9){
      std::cout << "Obesidade grau I" << std::endl;
  }
- if (35<= IMC && IMC <= 39.9){
+else if (IMC <= 39.9){
      std::cout << "Obesidade grau II" << std::endl;
  }
- if (IMC > 40){
+else {
      std::cout << " Obesidade grave/grau III" << std::endl;
  }    
- 
+
  return 0;   
 }
